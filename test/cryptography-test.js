9949d18e4e474e7cec54c43f68d04a3987d41ec9
tomascaelum <tomas@caelumlabs.com>
2020-05-14 11:19:11 +0200

    Minor changes

--- test/cryptography-test.js
+++ test/cryptography-test.js
@@ -9,17 +9,17 @@ const expect = chai.expect
 describe('Lorena Cryptography interface', function () {
   it('Should generate a correct class if all methods are implemented', () => {
     class TestCryptography extends CryptographyInterface {
-      randomDID(){}
-      hash(){}
-      random(){}
-      checkSignature(){}
-      signMessage(){}
-      decryptSymmetric(){}
-      encryptSymmetric(){}
-      decryptAsymmetric(){}
-      encryptAsymmetric(){}
-      newKeyPair(){}
-      publicKey(){}
+      randomDID () {}
+      hash () {}
+      random () {}
+      checkSignature () {}
+      signMessage () {}
+      decryptSymmetric () {}
+      encryptSymmetric () {}
+      decryptAsymmetric () {}
+      encryptAsymmetric () {}
+      newKeyPair () {}
+      publicKey () {}
     }
 
     const Cryptography = new TestCryptography()
